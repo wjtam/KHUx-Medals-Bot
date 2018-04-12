3030f6e38adc89fb5e6822e1964dff7d7d11fc0e
Wayne Tam <wayne0tam@gmail.com>
2018-04-12 19:59:33 -0400

    Add files via upload
    Updated bot to work on r/khux

---
+++ config.py
@@ -0,0 +1,4 @@
+username = "khux_medals_bot"
+password = "luffydragon321"
+client_id = "npk7pZBRdiJyjw"
+client_secret = "4HlMcsEZAExxayL--9ETv1oLfjk"
--- khux_medals_bot.py
+++ khux_medals_bot.py
@@ -1,6 +1,9 @@
 import praw
 import config
 import os
+import requests
+
+URL = 'https://www.khuxbot.com/api/v1/medal?q=data&filter='
 
 def bot_login():
 	r = praw.Reddit(username = config.username,
@@ -11,17 +14,45 @@ def bot_login():
 
 	return r
 
+def getMedal(str):
+	newStr = str.lower()
+	if newStr == 'xex' or newStr == 'ixex':
+		newStr = "illustrated xion (ex)"
+	if newStr == 'kex' or newStr == 'ikex':
+		newStr = "illustrated kh kairi (ex)"
+	if ' and ' in newStr:
+		newStr = newStr.replace(" and "," %26 ")
+	if '&' in newStr:
+		newStr = newStr.replace("&","%26")
+	if '[ex]' in newStr:
+		newStr = newStr.replace("[ex]","(ex)")
+	elif ' ex' in newStr:	
+		newStr = newStr.replace(" ex"," (ex)")
+
+	print('{\"name\":\"' + newStr + '\",\"rarity\":\"6\"}')
+
+	return '{\"name\":\"' + newStr + '\",\"rarity\":\"6\"}'
+
 def run_bot(r, comments_replied):
-
-	for comment in r.subreddit('test').comments(limit=25):
-		if comment.body.startswith('[[') and comment.body.endswith(']]') and comment.id not in comments_replied:
-			print ("String with \"khux\" found" + comment.id) 
-			comment.reply("I will able to show khux medals soon!")
-			print ("Replied to comment" + comment.id)
-
-			comments_replied.append(comment.id)
-			with open ("comments_replied.txt", "a") as f:
-				f.write(comment.id + "\n")
+	comment_found = False
+	for comment in r.subreddit('khux').comments(limit=25):
+		comment_body = comment.body.split()
+		for comment_word in comment_body:
+			if comment_word.startswith('{') and comment_word.endswith('}') and comment.id not in comments_replied:
+				comment_found = True
+				print ("String with \"{\" and \"}\" found" + comment.id) 
+				medalURL = getMedal(comment_word[1:-1])
+				data = requests.get(URL+medalURL).json()['medal']['0'] #medal json data
+				comment.reply('[**' + data['name'] + '**](https://www.khuxbot.com' + data['image_link'] + ') ' + data['direction'] 
+					+ ' ' + data['element'] + ' ' + str(data['tier']) + ' ' + data['targets'] 
+					+ '\n\n STR: ' + str(data['strength']) + ' DEF: ' + str(data['defence']) + ' MULT: ' + str(data['multiplier']) 
+					+ ' HITS: ' + str(data['hits']) + '\n\n'+ str(data['notes']))
+	
+	if comment_found:
+		print("Replied to comment" + comment.id)
+		comments_replied.append(comment.id)
+		with open ("comments_replied.txt", "a") as f:
+			f.write(comment.id + "\n")
 
 def get_saveds_comments():
 	if not os.path.isfile("comments_replied.txt"):
